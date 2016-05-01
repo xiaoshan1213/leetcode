4ebde1ff2c8d4c5ec16eea7bae7dd13fdce605d7
xiaoshan1213 <xiaoshan1213@gmail.com>
2016-05-01 16:46:02 +0800

    UniquePath

---
+++ UniquePath/src/com/sam/example/Solution.java
@@ -0,0 +1,21 @@
+package com.sam.example;
+
+public class Solution {
+	public int uniquePaths(int m, int n) {
+        int[][] grid=new int[m][n];
+        for(int i=0;i<m;i++){
+        	for(int j=0;j<n;j++){
+        		grid[i][j]=1;
+        	}
+        }
+        if(m==1 || n==1){
+        	return 1;
+        }
+        for(int i=1;i<m;i++){
+        	for(int j=1;j<n;j++){
+    			grid[i][j]=grid[i][j-1]+grid[i-1][j];
+        	}
+        }
+        return grid[m-1][n-1];
+    }
+}
