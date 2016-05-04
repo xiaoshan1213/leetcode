34d1d28bffb70572e14b784d050fea684b8f793e
xiaoshan1213 <xiaoshan1213@gmail.com>
2016-05-04 09:07:50 +0800

    DiffWaysToAddParent

---
+++ DifferentWaysToAddParantheses/src/com/sam/example/Solution.java
@@ -0,0 +1,41 @@
+package com.sam.example;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Solution {
+	Map<String,List<Integer>> map=new HashMap<>();
+	public List<Integer> diffWaysToCompute(String input) {
+        if(map.containsKey(input)){
+        	return map.get(input);
+        }
+        List<Integer> res=new ArrayList<>();
+        for(int i=0;i<input.length();i++){
+        	char c=input.charAt(i);
+        	if(c=='+' || c=='-' || c=='*'){
+        		List<Integer> list1=diffWaysToCompute(input.substring(0, i));
+        		List<Integer> list2=diffWaysToCompute(input.substring(i+1));
+        		for(int v1:list1){
+        			for(int v2:list2){
+        				if(c=='+'){
+        					res.add(v1+v2);
+        				}
+        				if(c=='-'){
+        					res.add(v1-v2);
+        				}
+        				if(c=='*'){
+        					res.add(v1*v2);
+        				}
+        			}
+        		}
+        	}
+        }
+        if(res.isEmpty()){
+        	res.add(Integer.parseInt(input));
+        }
+        map.put(input, res);
+        return res;
+    }
+}
